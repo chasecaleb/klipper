bcb60b6c6409e913ddd5d9510d07921a95e0d9d9
Caleb Chase <caleb@chasecaleb.com>
2019-10-10 17:37:41 -0500

    Finish psu script

--- toggle-psu.sh
+++ toggle-psu.sh
@@ -3,26 +3,21 @@ set -euo pipefail
 # Dependencies: "gpio" utility (included with raspbian/octopi)
 
 # These are the wiringPi-style header pin numbers, not true gpio pins.
-RAMPS_PIN=4
-PSU_PIN=5
-KLIPPER_SERIAL=/tmp/printer
+FAN_PIN=2
+PSU_PIN=4
 
 # Setup pins as outputs (if they aren't already)
-for pin in $PSU_PIN $RAMPS_PIN; do
-    # gpio mode "$pin" output
+for pin in $PSU_PIN $FAN_PIN; do
     gpio export "$pin" out
+    gpio mode "$pin" output
 done
 
 # Note: PSU needs to be first on, last off
 if [[ $# == 1 && $1 == "on" ]]; then
     gpio write $PSU_PIN 1
-    sleep 1
-    gpio write $RAMPS_PIN 1
-    echo firmware_restart > /tmp
-    sleep 3
+    gpio write $FAN_PIN 1
 elif [[ $# == 1 && $1 == "off" ]]; then
-    gpio write $RAMPS_PIN 0
-    sleep 1
+    gpio write $FAN_PIN 0
     gpio write $PSU_PIN 0
 else
     echo "Invalid arguments: expected either 'on' or 'off'"
